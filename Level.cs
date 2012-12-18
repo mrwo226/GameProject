cba554178d37b4437ed1e5b3f40938fb3490ae0c
Megan Worley <megan.worley@uky.edu>
2012-12-18 17:27:19 -0500

    Tile Engine 3 / Level System 1
    Made Level.cs an abstract class with the ability to draw tiles to the
    screen.
    Added LevelOne.cs, an inherited Level class that will serve as the first
    level for the game.

--- Level.cs
+++ Level.cs
@@ -11,49 +11,57 @@ using Microsoft.Xna.Framework.Graphics;
 
 namespace WindowsGame1
 {
-    class Level
+    public abstract class Level
     {
+        #region Fields
+
         Engine engine = new Engine();  // The engine used to hold tile information for this level.
-        Tileset tileset1; // The tileset that handles base images for this level.
-        Tileset tileset2; // A tileset to handle the second layer.
-        TileMap map; // The tilemap containing 
-        Texture2D tilesetTexture; // The tileset containing the images used for tiles.
-        ContentManager content; // A new content manager to load and unload content for the current level.
+        TileMap map; // The tilemap containing.
+        public Texture2D tilesetTexture; // The tileset containing the images used for tiles.
+        public List<Tileset> tilesets;
         SpriteBatch spriteBatch;
         Camera camera;
+        ContentManager content; // A new content manager to load and unload content for the current level.
+
+        #endregion
+
+        #region Properties
 
         public ContentManager Content
         {
             get { return content; }
         }
 
+        #endregion
+
+        #region Constructor
+
         // Constructs a level.  
-        public Level(IServiceProvider serviceProvider, Game1 game, Stream fileStream, int levelIndex, SpriteBatch spriteBatch)
+        public Level(IServiceProvider serviceProvider, Game1 game, Stream fileStream, SpriteBatch spriteBatch, int numLayers)
         {
+            this.spriteBatch = spriteBatch;
             // Create a new content manager to load content used just by this level.
             content = new ContentManager(serviceProvider, "Content");
-            this.spriteBatch = spriteBatch;
             LoadContent();
-            LoadTiles(fileStream);
+            LoadTiles(fileStream, numLayers);
             camera = new Camera(game.screenRectangle);
         }
+
+        #endregion
+
+        #region Methods
 
         /// <summary>
         /// Loads the images used for this level.
         /// </summary>
-        public void LoadContent()
+        public virtual void LoadContent()
         {
-            tilesetTexture = Content.Load<Texture2D>("Tilesets/tileset1");
-            tileset1 = new Tileset(tilesetTexture, 8, 8, 32, 32);
-
-            tilesetTexture = Content.Load<Texture2D>("Tilesets/tileset2");
-            tileset2 = new Tileset(tilesetTexture, 8, 8, 32, 32);
-            
+            tilesets = new List<Tileset>();
         }
 
-        public List<string> readFile(Stream fileStream)
+        public virtual List<string> readFile(Stream fileStream)
         {
-            // Load the level and ensure all the lines are the same length.
+            // Load each level from the text file line by line and ensure all the lines are the same length.
             int width;
             List<string> lines = new List<string>();
             using (StreamReader reader = new StreamReader(fileStream))
@@ -77,35 +85,39 @@ namespace WindowsGame1
         /// </summary>
         /// <param name="fileStream"></param>
         /// <param name="levelIndex"></param>
-        public void LoadTiles(Stream fileStream)
+        public virtual void LoadTiles(Stream fileStream, int numLayers)
         {
-            List<Tileset> tilesets = new List<Tileset>();
-            tilesets.Add(tileset1);
-            tilesets.Add(tileset2);
 
-            // Load the level and ensure all the lines are the same length.
+           // Load the level and ensure all the lines are the same length.
             List<string> lines = readFile(fileStream);
 
             // The level is as wide as the number of characters in a line minus the commas, and as high as the number of lines.
             int levelHeight = lines.Count;
             int width = lines[0].Length / 2;
-            MapLayer layer = new MapLayer(width, levelHeight);
+            List<MapLayer> mapLayers = new List<MapLayer>();
 
-            // Loop over every tile position and load each tile
-            for (int y = 0; y < layer.Height; y++)
+            // Loop through every single layer.
+            for (int i = 0; i < numLayers; i++)
             {
-                string[] tileNumbers = lines[y].Split(',');
-                for (int x = 0; x < layer.Width; x++)
+                // Create a new layer to assign tiles to.
+                MapLayer layer = new MapLayer(width, levelHeight);
+
+                // Loop over every tile position in the file and set each tile to the layer.
+                for (int y = 0; y < layer.Height; y++)
                 {
-                    // Use the numbers from the file by converting them to integers.
-                    int tileIndex = Convert.ToInt32(tileNumbers[x]);
-                    Tile tile = new Tile(tileIndex - 1, 0);
-                    layer.SetTile(x, y, tile);
+                    string[] tileNumbers = lines[y].Split(',');
+                    for (int x = 0; x < layer.Width; x++)
+                    {
+                        // Use the numbers from the file by converting them to integers.
+                        int tileIndex = Convert.ToInt32(tileNumbers[x]);
+                        Tile tile = new Tile(tileIndex - 1, 0);
+                        layer.SetTile(x, y, tile);
+                    }
                 }
+                mapLayers.Add(layer);
             }
 
-            List<MapLayer> mapLayers = new List<MapLayer>();
-            mapLayers.Add(layer);
+            // Create the tilemap with the tilesets and the layers.
             map = new TileMap(tilesets, mapLayers);
         }
 
@@ -115,14 +127,26 @@ namespace WindowsGame1
             Content.Unload();
         }
 
+        #endregion
+
+        #region Update
+
+        // Updates the camera scrolling.
         public void Update(GameTime gameTime)
         {
             camera.Update(gameTime);
         }
 
+        #endregion
+
+        #region Draw
+
+        // Draws the map to the screen.
         public void Draw(SpriteBatch spriteBatch)
         {
             map.Draw(spriteBatch, camera);
         }
+
+        #endregion
     }
 }
---
+++ LevelOne.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Content;
+using System.IO;
+using XnaActionLibrary.TileEngine;
+
+namespace WindowsGame1
+{
+    public class LevelOne : Level
+    {
+        Tileset tileset1; //
+        Tileset tileset2;
+
+        // Constructs a level.  
+        public LevelOne(IServiceProvider serviceProvider, Game1 game, Stream fileStream, SpriteBatch spriteBatch, int numLayers) 
+            : base(serviceProvider, game, fileStream, spriteBatch, numLayers)
+        {
+        }
+
+        /// <summary>
+        /// Loads the images used for this level.
+        /// </summary>
+        public override void LoadContent()
+        {
+            base.LoadContent();
+
+            tilesetTexture = Content.Load<Texture2D>("Tilesets/tileset1");
+            tileset1 = new Tileset(tilesetTexture, 8, 8, 32, 32);
+
+            tilesetTexture = Content.Load<Texture2D>("Tilesets/tileset2");
+            tileset2 = new Tileset(tilesetTexture, 8, 8, 32, 32);
+
+            tilesets.Add(tileset1);
+            tilesets.Add(tileset2);
+
+        }
+
+        // Unloads the level content
+        public void Dispose()
+        {
+            Content.Unload();
+        }
+    }
+}
